8fcbabb0f0e0605f13b265482215c3fcdcd08e80
TECHTHUNDER2018 <nishant999908@gmail.com>
2026-01-31 11:44:46 +0530

    LESSON LEARN

--- PATTERN/pattern2.java
+++ PATTERN/pattern2.java
@@ -2,7 +2,7 @@ package PATTERN;
 
 public class pattern2 {
     public static void main(String[] args){
- int n = 5, m =5;
+ int n = 5;
  for(int i = 0; i<n; i++){
     for(int j = 0 ; j<= i ; j++){
      System.out.print("*");
