af6b338536a99d068bc32c49a4d670c0f39d1dcc
TECHTHUNDER2018 <nishant999908@gmail.com>
2026-01-31 23:09:50 +0530

    todays target acheived

---
+++ PATTERN/invertedstarpyramid.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class invertedstarpyramid {
+    public static void main(String[] args){
+
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+for(int i = 0 ; i< n ; i++){
+    for(int j = n; j>= n-i; j--){
+        System.out.print(" ");
+    }
+    for(int j = n ; j>=2*i+1 ; j--){
+        System.out.print("*");
+    }
+    for(int j = n; j>= n-i; j--){
+        System.out.print(" ");
+    }
+
+
+
+System.out.println();
+}
+
+
+
+
+
+
+
+
+
+        sc.close();
+    }
+}
