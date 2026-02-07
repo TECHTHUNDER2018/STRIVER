4be1a02a1e9c63d58f354ca30b351419eb75b5a7
TECHTHUNDER2018 <nishant999908@gmail.com>
2026-02-07 19:21:08 +0530

    LESSON LEARNT

--- PATTERN/invertedstarpyramid.java
+++ PATTERN/invertedstarpyramid.java
@@ -5,15 +5,16 @@ public class invertedstarpyramid {
 Scanner sc = new Scanner(System.in);
 int n = sc.nextInt();
 for(int i = 0 ; i< n ; i++){
-    for(int j = n; j>= n-i; j--){
+    for(int j = 0; j< i; j++){
+        System.out.print(" ");}
+    for(int j = 0 ;  j<2*n -(2*i+1) ; j++){
+ System.out.print("*");
+        }
+       
+    for(int j = 0 ; j <i ; j++){
         System.out.print(" ");
     }
-    for(int j = n ; j>=2*i+1 ; j--){
-        System.out.print("*");
-    }
-    for(int j = n; j>= n-i; j--){
-        System.out.print(" ");
-    }
+    
 
 
 
---
+++ PATTERN/rightpyramidbinary.java
@@ -0,0 +1,34 @@
+import java.util.*;public class rightpyramidbinary {
+    public static void main( String[] args){
+
+Scanner sc = new Scanner(System.in);
+int n = sc.nextInt();
+ for(int i =0 ; i<n ;i++){
+    if(i%2 == 0){
+    for(int j =0 ; j <i ; j++){
+if(j%2 == 0){
+    System.out.print("0");
+}else{
+    System.out.print("1");
+}
+    }
+    System.out.println();
+
+}else{
+    for(int j = 0; j <i ; j++){
+       if(j%2 == 0){ System.out.print("1");
+    }else{
+        System.out.print("0");
+    }
+ }
+System.out.println();
+
+
+ }
+
+
+
+
+sc.close();
+    }}
+}
